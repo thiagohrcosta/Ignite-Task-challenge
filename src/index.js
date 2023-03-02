bd3c447636950b7902462ae0ea44768f47db3591
Thiagohrcosta <thiagohrcosta86@gmail.com>
2023-03-01 23:08:41 -0300

    check if username already exist

--- src/index.js
+++ src/index.js
@@ -11,10 +11,20 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body;
+
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    return response.status(404).json({
+      error: "User already exists!"
+    })
+  }
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
+app.post('/users', checksExistsUserAccount, (request, response) => {
   const { name, username } = request.body;
 
   users.push({
